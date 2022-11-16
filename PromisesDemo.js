86c4836485c3ca4c3451524dcd73763c6f1b0f0f
RutujaPatil18 <prutuja189@gmail.com>
2022-11-16 23:03:07 +0530

    Add:Generate random number base on that reject or resolve promise

---
+++ PromisesDemo.js
@@ -0,0 +1,26 @@
+/*
+console.log(x);*/
+// returns a promise
+let countValue = new Promise(function (resolve, reject) {
+    let min = 1;
+    let max = 2;
+    const random = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (random == 1) {
+        resolve("Promise resolved");
+    } else {
+        reject('Promise rejected');
+    }
+});
+
+// executes when promise is resolved successfully
+countValue.then(
+    function successValue(result) {
+        console.log(result);
+    },
+)
+    // executes if there is an error
+    .catch(
+        function errorValue(result) {
+            console.log(result);
+        }
+    );
